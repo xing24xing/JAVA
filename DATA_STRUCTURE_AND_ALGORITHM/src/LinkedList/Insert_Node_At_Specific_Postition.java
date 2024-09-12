bcefb6f2da08a538f0a9f1c411b83fff3df8826c
xing24xing <khushipandey260304@gmail.com>
2024-09-12 23:53:51 +0530

    Perform Insert And Delete Operation in Linked List

--- DATA_STRUCTURE_AND_ALGORITHM/src/LinkedList/Insert_Node_At_Specific_Postition.java
+++ DATA_STRUCTURE_AND_ALGORITHM/src/LinkedList/Insert_Node_At_Specific_Postition.java
@@ -35,6 +35,30 @@ public class Insert_Node_At_Specific_Postition {
        return head;
     
 }
+    public static Node delete(Node head,int pos){
+         
+        if (head == null) {
+            System.out.println("List is empty");
+            return null;
+        }
+        if (pos == 1) {
+            head = head.next;
+            return head;
+        }
+       Node t = head;
+       for(int i = 1;i < pos - 1 && t != null;i++)
+       {
+           t = t.next;
+       }
+       if(t == null || t.next == null){
+           System.out.println("Position Is Out Of The Bound");
+           return head;
+       }
+      t.next = t.next.next;
+       return head;
+       
+}
+
     public static void print(Node h){
         while(h != null){
             System.out.print(h.data+" -> ");
@@ -47,11 +71,16 @@ public class Insert_Node_At_Specific_Postition {
         Node n = new Node(4);
         n.next = new Node(5);
         n.next.next = new Node(6);
+        n.next.next = new Node(8);
+        n.next.next = new Node(7);
         System.out.println("Original");
         print(n);
         System.out.println("After Insertion");
         int p = 3,val = 12;
         n = insert(n,p,val);
+        print(n);
+        System.out.println("After Deletion:");
+        n = delete(n, 2);
         print(n);
         
     }
